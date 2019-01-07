6e9366f2348d90a7c5df0fb8461107ff8225b2b0
Dustin Hartenstein <cooldustin99@yahoo.com>
2019-01-07 13:22:17 -0500

    More Design

---
+++ pocket-calculator.js
@@ -0,0 +1,76 @@
+function clear() {
+
+}
+
+function switch() {
+
+}
+
+function percent() {
+
+}
+
+function divide() {
+
+}
+
+function seven() {
+  let p=document.getElementById("boxed");
+  p.innerHTML="7";
+}
+
+function eight() {
+
+}
+
+function nine() {
+
+}
+
+function multiply() {
+
+}
+
+function four() {
+
+}
+
+function five() {
+
+}
+
+function six() {
+
+}
+
+function minus() {
+
+}
+
+function one() {
+
+}
+
+function two() {
+
+}
+
+function three() {
+
+}
+
+function add() {
+
+}
+
+function decimal() {
+
+}
+
+function zero() {
+
+}
+
+function equals() {
+
+}
