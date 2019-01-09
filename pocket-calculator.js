99b4c0723a538fbe96527c701701412d160ef49d
Dustin Hartenstein <cooldustin99@yahoo.com>
2019-01-09 13:12:41 -0500

    Buttons and Screen Works

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,76 +1,94 @@
 function clear() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="clear";
 }
 
-function switch() {
-
+function flop() {
+  let p=document.getElementById("screen");
+  p.innerHTML="+/-";
 }
 
 function percent() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="%";
 }
 
 function divide() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="÷";
 }
 
 function seven() {
-  let p=document.getElementById("boxed");
+  let p=document.getElementById("screen");
   p.innerHTML="7";
 }
 
 function eight() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="8";
 }
 
 function nine() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="9";
 }
 
 function multiply() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="x";
 }
 
 function four() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="4";
 }
 
 function five() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="5";
 }
 
 function six() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="6";
 }
 
 function minus() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="-";
 }
 
 function one() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="1";
 }
 
 function two() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="2";
 }
 
 function three() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="3";
 }
 
 function add() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="+";
 }
 
 function decimal() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML=".";
 }
 
 function zero() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="0";
 }
 
 function equals() {
-
+  let p=document.getElementById("screen");
+  p.innerHTML="=";
 }
