d1f1e5f153ac595a89fe50ecfff28513ecbdac23
Dustin Hartenstein <cooldustin99@yahoo.com>
2019-01-11 13:23:00 -0500

    Stripes not working

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,94 +1,138 @@
-function clear() {
+let numbers=[];
+
+function rid() {
   let p=document.getElementById("screen");
-  p.innerHTML="clear";
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  numbers.pop();
+  p.innerHTML="0";
 }
 
 function flop() {
   let p=document.getElementById("screen");
-  p.innerHTML="+/-";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function percent() {
   let p=document.getElementById("screen");
-  p.innerHTML="%";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function divide() {
   let p=document.getElementById("screen");
-  p.innerHTML="÷";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function seven() {
+  if(numbers.length<9) {
+    numbers.push(7)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="7";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function eight() {
+  if(numbers.length<9) {
+    numbers.push(8)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="8";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function nine() {
+  if(numbers.length<9) {
+    numbers.push(9)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="9";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function multiply() {
   let p=document.getElementById("screen");
-  p.innerHTML="x";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function four() {
+  if(numbers.length<9) {
+    numbers.push(4)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="4";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function five() {
+  if(numbers.length<9) {
+    numbers.push(5)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="5";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function six() {
+  if(numbers.length<9) {
+    numbers.push(6)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="6";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function minus() {
   let p=document.getElementById("screen");
-  p.innerHTML="-";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function one() {
+  if(numbers.length<9) {
+    numbers.push(1)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="1";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function two() {
+  if(numbers.length<9) {
+    numbers.push(2)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="2";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function three() {
+  if(numbers.length<9) {
+    numbers.push(3)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="3";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function add() {
   let p=document.getElementById("screen");
-  p.innerHTML="+";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function decimal() {
+  if(numbers.length<9) {
+    numbers.push(".")
+  }
   let p=document.getElementById("screen");
-  p.innerHTML=".";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function zero() {
+  if(numbers.length<9) {
+    numbers.push(0)
+  }
   let p=document.getElementById("screen");
-  p.innerHTML="0";
+  p.innerHTML=`${numbers.join("")}`;
 }
 
 function equals() {
   let p=document.getElementById("screen");
-  p.innerHTML="=";
+  p.innerHTML=`${numbers.join("")}`;
 }
