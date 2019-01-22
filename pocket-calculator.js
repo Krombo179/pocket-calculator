bf0ba7531af7ca1c0df81f713b4c3a6bcac02e0b
Dustin Hartenstein <cooldustin99@yahoo.com>
2019-01-22 13:22:59 -0500

    Almost Done

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,149 +1,141 @@
-let numbers=[];
-let decimals=false;
+let answer="";
+let works="";
+let standby="";
+let temporaryorary="";
+let count=0
+
+function n(num) {
+  if(document.getElementById("screen").innerHTML=="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/") {
+    works+=document.getElementById("screen").innerHTML
+    document.getElementById("screen").innerHTML=""
+  } if (document.getElementById("screen").innerHTML=="0") {
+    document.getElementById("screen").innerHTML=""
+  } if (document.getElementById("screen").innerHTML==answer) {
+    document.getElementById("screen").innerHTML=""
+  } if (standby.length > 8) {
+  } else {
+    standby=standby+num
+    temporary = document.getElementById("screen").innerHTML+num
+    document.getElementById("screen").innerHTML = temporary.toLocaleString();
+    if (document.getElementById("screen").innerHTML=="-"+standby) {
+    } else {
+      standby = document.getElementById("screen").innerHTML
+    }
+  }
+}
 
 function rid() {
-  let p=document.getElementById("screen");
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  numbers.pop();
-  p.innerHTML="0";
-  decimalClear()
-}
-
-function flop() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function percent() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function divide() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-  decimalClear();
-}
-
-function seven() {
-  if(numbers.length<9) {
-    numbers.push(7)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function eight() {
-  if(numbers.length<9) {
-    numbers.push(8)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function nine() {
-  if(numbers.length<9) {
-    numbers.push(9)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function multiply() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-  decimalClear();
-}
-
-function four() {
-  if(numbers.length<9) {
-    numbers.push(4)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function five() {
-  if(numbers.length<9) {
-    numbers.push(5)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function six() {
-  if(numbers.length<9) {
-    numbers.push(6)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function minus() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-  decimalClear();
-}
-
-function one() {
-  if(numbers.length<9) {
-    numbers.push(1)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function two() {
-  if(numbers.length<9) {
-    numbers.push(2)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function three() {
-  if(numbers.length<9) {
-    numbers.push(3)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-}
-
-function add() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-  decimalClear();
-}
-
-function decimal() {
-  if(numbers.length<9 && decimals==false) {
-    numbers.push(".")
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
-  decimals=true;
-}
-
-function zero() {
-  if(numbers.length<9) {
-    numbers.push(0)
-  }
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
+  document.getElementById("screen").innerHTML="0"
+  answer=""
+  works=""
+  standby=""
 }
 
 function equals() {
-  let p=document.getElementById("screen");
-  p.innerHTML=`${numbers.join("")}`;
+  if (document.getElementById("screen").innerHTML =="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/") {
+  } else {
+  works+=standby
+  standby=""
+  answer=eval(works)
+  document.getElementById("screen").innerHTML=answer.toLocaleString();
+  if (answer=="Infinity") {
+    document.getElementById("screen").innerHTML="Infinity is Invalid"
+  } else if (answer>=1000000000) {
+    let exponent=answer.toString().length-1;
+    answer=answer/(10 ** (exponent))
+    document.getElementById('screen').innerHTML=answer.toFixed(7)+'e'+exponent;
+    }
+  }
 }
 
-function decimalClear() {
-  decimals==false
+function flop() {
+  if (document.getElementById("screen").innerHTML=="-"+standby) {
+    document.getElementById("screen").innerHTML=standby
+  } else if (document.getElementById("screen").innerHTML.indexOf("-")==0&&document.getElementById("screen").innerHTML!="-"+ answer) {
+    document.getElementById("screen").innerHTML=standby
+  } else if (document.getElementById("screen").innerHTML==answer){
+    document.getElementById("screen").innerHTML="-"+answer
+  } else if (document.getElementById("screen").innerHTML=="-"+answer){
+    document.getElementById("screen").innerHTML=answer
+  } else {
+    document.getElementById("screen").innerHTML="-"+standby
+  }
+}
+
+function percent() {
+  if (document.getElementById("screen").innerHTML.indexOf("-")==0) {
+    standby=eval(standby*0.01).toFixed(7)
+    document.getElementById("screen").innerHTML="-"+standby
+  } else {
+    standby=eval(standby*0.01).toFixed(7)
+    document.getElementById("screen").innerHTML=standby
+  }
+}
+
+function decimal() {
+  if (document.getElementById("screen").innerHTML.indexOf(".")!==-1){
+    document.getElementById("screen").innerHTML=document.getElementById("screen").innerHTML
+    } else {
+    document.getElementById("screen").innerHTML+="."
+    }
+  }
+
+function add() {
+  if (document.getElementById("screen").innerHTML=="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/") {
+    document.getElementById("screen").innerHTML="+"
+  } else if (standby="") {
+  } else if (document.getElementById("screen").innerHTML==answer.toLocaleString()) {
+    works=answer
+    document.getElementById("screen").innerHTML ="+"
+  } else {
+    works+=document.getElementById("screen").innerHTML
+    standby=""
+    document.getElementById("screen").innerHTML = "+"
+  }
+}
+
+function minus() {
+  if(document.getElementById("screen").innerHTML=="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/") {
+  document.getElementById("screen").innerHTML="-"
+  } else if (standby=""){
+  } else if (document.getElementById("screen").innerHTML==answer.toLocaleString()) {
+    works=answer
+    document.getElementById("screen").innerHTML="-"
+  } else {
+    works+=document.getElementById("screen").innerHTML
+    standby=""
+    document.getElementById("screen").innerHTML="-"
+  }
+}
+
+function multiply() {
+  if (document.getElementById("screen").innerHTML=="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/") {
+    document.getElementById("screen").innerHTML="*"
+  } else if (standby=""){
+  } else if (document.getElementById("screen").innerHTML==answer.toLocaleString()){
+    works=answer
+    document.getElementById("screen").innerHTML="*"
+  } else {
+    works+=document.getElementById("screen").innerHTML
+    standby=""
+    document.getElementById("screen").innerHTML="*"
+  }
+}
+
+function divide() {
+  if (document.getElementById("screen").innerHTML=="+"||document.getElementById("screen").innerHTML=="-"||document.getElementById("screen").innerHTML=="*"||document.getElementById("screen").innerHTML=="/"){
+    document.getElementById("screen").innerHTML="/"
+  } else if (standby=""){
+  } else if (document.getElementById("screen").innerHTML==answer.toLocaleString()){
+    works=answer
+    document.getElementById("screen").innerHTML="/"
+  } else {
+    works+=document.getElementById("screen").innerHTML
+    standby=""
+    document.getElementById("screen").innerHTML="/"
+  }
+}
+
+function website() {
+  window.open("https://www.off---white.com/");
 }
